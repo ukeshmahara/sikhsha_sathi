961669ab6c6613b3d9bc43e3b5cb769ad78c1b1b
ukeshmahara <ukeshmahara361@gmail.com>
2026-06-05 23:20:06 +0545

    Implement auth feature with Hive and clean architecture

---
+++ android/app/src/main/kotlin/com/ukesh/sikhsha_sathi/sikhsha_sathi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ukesh.sikhsha_sathi.sikhsha_sathi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
-import 'package:flutter/material.dart';
-import 'app.dart';
 
-void main(){
-  runApp(const App());
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'app/app.dart';
+
+import 'core/services/hive/hive_service.dart';
+
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Hive Service
+  final hiveService = HiveService();
+
+  await hiveService.init();
+
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
 }
