f09d0db699f8dbf877632ec0511a1660df8000e6
ukeshmahara <ukeshmahara361@gmail.com>
2026-06-16 21:27:33 +0545

    Initialized shared preferences on startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,32 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app/app.dart';
 
 import 'core/services/hive/hive_service.dart';
+import 'core/services/storage/user_session_service.dart';
 
 Future<void> main() async {
 
-  WidgetsFlutterBinding.ensureInitialized();
+WidgetsFlutterBinding.ensureInitialized();
 
-  // Initialize Hive Service
-  final hiveService = HiveService();
+// Initialize Hive
+await HiveService().init();
 
-  await hiveService.init();
+// Initialize SharedPreferences
+final sharedPreferences =
+await SharedPreferences.getInstance();
 
-  runApp(
-    const ProviderScope(
-      child: App(),
-    ),
-  );
+runApp(
+ProviderScope(
+overrides: [
+sharedPreferencesProvider
+.overrideWithValue(
+sharedPreferences,
+),
+],
+child: const App(),
+),
+);
 }
