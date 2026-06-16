31ca917bc1e61506b8710ac598a546bd40e4a714
ukeshmahara <ukeshmahara361@gmail.com>
2026-06-16 21:26:55 +0545

    Added user session management using shared preferences

---
+++ lib/core/services/storage/user_session_service.dart
@@ -0,0 +1,113 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final sharedPreferencesProvider =
+    Provider<SharedPreferences>((ref) {
+  throw UnimplementedError();
+});
+
+final userSessionServiceProvider =
+    Provider<UserSessionService>((ref) {
+  final prefs =
+      ref.read(sharedPreferencesProvider);
+
+  return UserSessionService(
+    prefs: prefs,
+  );
+});
+
+class UserSessionService {
+  final SharedPreferences _prefs;
+
+  UserSessionService({
+    required SharedPreferences prefs,
+  }) : _prefs = prefs;
+
+  static const String _isLoggedIn =
+      "is_logged_in";
+
+  static const String _userId =
+      "user_id";
+
+  static const String _email =
+      "email";
+
+  static const String _fullName =
+      "full_name";
+
+  static const String _phoneNumber =
+      "phone_number";
+
+  Future<void> saveUserSession({
+    required String userId,
+    required String email,
+    required String fullName,
+    String? phoneNumber,
+  }) async {
+    await _prefs.setBool(
+      _isLoggedIn,
+      true,
+    );
+
+    await _prefs.setString(
+      _userId,
+      userId,
+    );
+
+    await _prefs.setString(
+      _email,
+      email,
+    );
+
+    await _prefs.setString(
+      _fullName,
+      fullName,
+    );
+
+    if (phoneNumber != null) {
+      await _prefs.setString(
+        _phoneNumber,
+        phoneNumber,
+      );
+    }
+  }
+
+  bool isLoggedIn() {
+    return _prefs.getBool(
+          _isLoggedIn,
+        ) ??
+        false;
+  }
+
+  String? getUserId() {
+    return _prefs.getString(
+      _userId,
+    );
+  }
+
+  String? getEmail() {
+    return _prefs.getString(
+      _email,
+    );
+  }
+
+  String? getFullName() {
+    return _prefs.getString(
+      _fullName,
+    );
+  }
+
+  String? getPhoneNumber() {
+    return _prefs.getString(
+      _phoneNumber,
+    );
+  }
+
+  Future<void> clearSession() async {
+    await _prefs.remove(_isLoggedIn);
+    await _prefs.remove(_userId);
+    await _prefs.remove(_email);
+    await _prefs.remove(_fullName);
+    await _prefs.remove(_phoneNumber);
+  }
+}
